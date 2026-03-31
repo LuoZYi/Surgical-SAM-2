aea3514c5866d57321dc9e3537946fe11ce5a6b5
LuoZYi <luozhongyi2002@gmail.com>
2026-04-01 04:52:40 +0800

    updated slurm log location

--- shell/endovis2017/vos_2017_off.sh
+++ shell/endovis2017/vos_2017_off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=vos_2017_off
-#SBATCH --output=/home/e/e0968951/fyp/Surgical-SAM-2/endovis2017/logs/vos_2017_off_%j.out
-#SBATCH --error=/home/e/e0968951/fyp/Surgical-SAM-2/endovis2017/logs/vos_2017_off_%j.err
+#SBATCH --output=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2017/logs/vos_2017_off_%j.out
+#SBATCH --error=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2017/logs/vos_2017_off_%j.err
 #SBATCH --time=03:00:00           # 建议和你salloc的时间保持一致
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=256G                # 对应你salloc中的256G
--- shell/endovis2018/vos_2018_off.sh
+++ shell/endovis2018/vos_2018_off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=vos_2018_off
-#SBATCH --output=/home/e/e0968951/fyp/Surgical-SAM-2/endovis2018/logs/vos_2018_off_%j.out
-#SBATCH --error=/home/e/e0968951/fyp/Surgical-SAM-2/endovis2018/logs/vos_2018_off_%j.err
+#SBATCH --output=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2018/logs/vos_2018_off_%j.out
+#SBATCH --error=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2018/logs/vos_2018_off_%j.err
 #SBATCH --time=03:00:00           
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=256G                
