fa646cd958b095a6e3a5fe2c9c3f0562d6829f0a
LuoZYi <luozhongyi2002@gmail.com>
2026-04-02 05:47:06 +0800

    added state aware mode

---
+++ shell/endovis2017/vos_2017_state.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=vos_2017_stateaware
+#SBATCH --output=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2017/logs/vos_2017_stateaware_%j.out
+#SBATCH --error=/home/e/e0968951/fyp/Surgical-SAM-2/shell/endovis2017/logs/vos_2017_stateaware_%j.err
+#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=256G
+#SBATCH --gres=gpu:h100-47:1
+#SBATCH --constraint=xgpi
+
+set -e
+set -x
+
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export PYTHONUNBUFFERED=1
+
+cd /home/e/e0968951/fyp/Surgical-SAM-2
+export PYTHONPATH=/home/e/e0968951/fyp/Surgical-SAM-2:$PYTHONPATH
+
+# 创建结果保存目录
+mkdir -p /home/e/e0968951/fyp/Surgical-SAM-2/results/vos_2017_stateaware
+
+/home/e/e0968951/fyp/fyp_env/bin/python tools/vos_inference.py \
+  --sam2_cfg configs/sam2.1/sam2.1_hiera_s.yaml \
+  --sam2_checkpoint ./checkpoints/sam2.1_hiera_s_endo18.pth \
+  --output_mask_dir ./results/vos_2017_stateaware \
+  --input_mask_dir ./dataset/VOS-Endovis17/valid/VOS/Annotations_vos_instrument \
+  --base_video_dir ./dataset/VOS-Endovis17/valid/JPEGImages \
+  --gt_root ./dataset/VOS-Endovis17/valid/Annotations \
+  --gpu_id 0 \
+  --video_list_file ./video_txt/2017_val.txt \
+  --memory_prune_mode state_aware \
+  --memory_score_mode cosine_motion_geometry \
+  --num_frame_to_prune 2 \
+  --protect_conditioning_memories
