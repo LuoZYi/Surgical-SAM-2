40d6e063db650a118d5e963122908ccc88b9e749
lzy <zhongyi.luo@u.nus.edu>
2026-04-07 08:17:16 +0800

    minor updates

--- shell/endovis2018/train_write_controller_online_rl_efp_endo18.sh
+++ shell/endovis2018/train_write_controller_online_rl_efp_endo18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_rl_efp_endo18_2018
-#SBATCH --output=shell/endovis2018/logs/train_write_controller_online_rl_efp_endo18_%j.out
-#SBATCH --error=shell/endovis2018/logs/train_write_controller_online_rl_efp_endo18_%j.err
+#SBATCH --output=logs/train_write_controller_online_rl_efp_endo18_%j.out
+#SBATCH --error=logs/train_write_controller_online_rl_efp_endo18_%j.err
 #SBATCH --time=24:00:00        
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=256G                
